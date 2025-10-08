ff6869a35b3ee6222100a70748b870f10a51e4c8
Hanane HB <hananebh146@gmail.com>
2025-10-08 10:14:09 -0500

    la clase ProductosController creada

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
@@ -0,0 +1,10 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/productos") // le estpy diciendo que apartir de /productos vamos a trabajar
+public class ProductoController {
+    
+}
