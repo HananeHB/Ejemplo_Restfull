945a85af4c39228a6af8e3affdb766b7369864f6
Hanane HB <hananebh146@gmail.com>
2025-10-14 13:51:28 -0500

    clase FindProductoUseCase creada

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
@@ -1,7 +1,10 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.rest;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +30,7 @@ public class ProductoController {
 		Producto producto = createProductoService.createProducto(comando);
 		return ResponseEntity.status(HttpStatus.CREATED).body(ProductoMapper.toResponse(producto)); //Respuestagit@github.com:julparper/dawes-springboot-restful.git
 	}
+
+    @GetMapping
+public List<ProductoResponse> allProductos();
 }
