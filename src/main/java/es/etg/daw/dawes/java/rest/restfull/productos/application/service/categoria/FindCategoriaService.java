b843964e49f6ca69119e4f80594adee83de48bf4
HananeHB <hananebh146@gmail.com>
2025-11-05 06:37:12 -0600

    pequeñas modificaciones y clase CategoriaConfig implementada

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/service/categoria/FindCategoriaService.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/service/categoria/FindCategoriaService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import es.etg.daw.dawes.java.rest.restfull.productos.application.usecase.categoria.FindeCategoriaUseCase;
+import es.etg.daw.dawes.java.rest.restfull.productos.application.usecase.categoria.FindCategoriaUseCase;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Categoria;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
 public class FindCategoriaService extends CategoriaService{
-    private final FindeCategoriaUseCase findeCategoriaUseCase;
+    private final FindCategoriaUseCase findeCategoriaUseCase;
 
     @Cacheable //Cacheamos la lista LISTA | COLECCION
     public List<Categoria> findAll() {
