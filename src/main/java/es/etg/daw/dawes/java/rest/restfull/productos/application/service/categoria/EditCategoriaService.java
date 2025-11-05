acd53bbeddbfd96977eb87f01f05236ee597937d
HananeHB <hananebh146@gmail.com>
2025-11-05 05:14:02 -0600

    clases de service  de categoria creadas

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/service/categoria/EditCategoriaService.java
@@ -0,0 +1,8 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.application.service.categoria;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EditCategoriaService {
+    
+}
--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/categoria/DeleteCategoriaUseCase.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/categoria/DeleteCategoriaUseCase.java
@@ -1,5 +1,10 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase.categoria;
 
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class DeleteCategoriaUseCase {
-    
+    private CategoriaRepository categoriaRepository;
+
 }
