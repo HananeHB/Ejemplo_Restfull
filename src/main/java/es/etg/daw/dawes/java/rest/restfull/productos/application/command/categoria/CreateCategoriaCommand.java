3a4a482e2771f0e93748243e56686e34328f03a6
HananeHB <hananebh146@gmail.com>
2025-11-05 05:23:17 -0600

    clases coomand  de categoria creadas

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/command/categoria/CreateCategoriaCommand.java
@@ -0,0 +1,12 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@AllArgsConstructor
+@Accessors(fluent = true) // Así los getters no llevan prefijo get
+public class CreateCategoriaCommand {
+    private String nombre;
+}
---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/command/categoria/EditCategoriaCommand.java
@@ -0,0 +1,14 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria;
+
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.CategoriaId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@AllArgsConstructor
+@Accessors(fluent = true) // Así los getters no llevan prefijo get
+public class EditCategoriaCommand {
+    private CategoriaId id;
+    private String nombre;
+}
