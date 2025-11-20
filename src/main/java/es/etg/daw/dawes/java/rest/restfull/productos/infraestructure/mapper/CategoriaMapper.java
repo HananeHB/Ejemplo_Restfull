70ed0d8dbcc6bdedb832e1fc078b98ea2c457f5b
HananeHB <hananebh146@gmail.com>
2025-11-20 06:07:15 -0600

    Mapper de Categoria modificado

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/mapper/CategoriaMapper.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/mapper/CategoriaMapper.java
@@ -1,9 +1,13 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria.CreateCategoriaCommand;
 import es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria.EditCategoriaCommand;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Categoria;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.CategoriaId;
+import es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.jpa.entity.CategoriaEntity;
 import es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.dto.categoria.CategoriaRequest;
 import es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.dto.categoria.CategoriaResponse;
 
@@ -21,4 +25,19 @@ public class CategoriaMapper {
 	public static EditCategoriaCommand toCommand(CategoriaId id, CategoriaRequest categoriaRequest){
 		return new EditCategoriaCommand(id, categoriaRequest.nombre());
 	}
+
+	public static Categoria toDomain(CategoriaEntity p){
+        return Categoria.builder().id(new CategoriaId(p.getId()))
+                                 .nombre(p.getNombre())
+                                 .build();
+
+    }
+
+    public static List<Categoria> toDomain(List<CategoriaEntity> lista){
+        List<Categoria> lc = new ArrayList<>();
+        for(CategoriaEntity ce: lista){
+            lc.add(toDomain(ce));
+        }
+        return lc;
+    }
 }
