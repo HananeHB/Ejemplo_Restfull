5f6ad92f1ff6007cd40d9764e71e4c1a14f446ed
HananeHB <hananebh146@gmail.com>
2025-11-20 05:31:12 -0600

    RepositoriosJpa de producto y categoria hechos(las interfaces)

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/db/jpa/repository/CategoriaJpaRepository.java
@@ -0,0 +1,12 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.db.jpa.entity.CategoriaEntity;
+
+@Repository
+public interface CategoriaJpaRepository extends JpaRepository<CategoriaEntity, Integer>{
+    // Hereda automáticamente métodos como: save(), findById(), findAll(), delete(), etc.
+
+}
